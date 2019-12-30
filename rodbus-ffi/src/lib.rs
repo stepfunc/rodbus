f3c6ee637ff1587149592702e715a724f44e6410
jadamcrain <jadamcrain@gmail.com>
2019-12-29 17:55:35 -0800

    make a static library

--- rodbus-ffi/src/lib.rs
+++ rodbus-ffi/src/lib.rs
@@ -3,6 +3,8 @@ use std::net::SocketAddr;
 use std::ptr::null_mut;
 use std::str::FromStr;
 
+
+
 #[no_mangle]
 pub extern "C" fn create_tcp_client(
     address: *const std::os::raw::c_char,
@@ -11,27 +13,31 @@ pub extern "C" fn create_tcp_client(
     // if we can't turn the c-string into SocketAddr, return null
     let addr = {
         match unsafe { CStr::from_ptr(address) }.to_str() {
+            // TODO - consider logging?
             Err(_) => return null_mut(),
             Ok(s) => match SocketAddr::from_str(s) {
+                // TODO - consider logging?
                 Err(_) => return null_mut(),
                 Ok(addr) => addr,
             },
         }
     };
 
-    let boxed = Box::new(rodbus::client::spawn_tcp_client_task(
-        addr,
-        max_queued_requests,
-        rodbus::client::channel::strategy::default(),
-    ));
+    let boxed = Box::new(
+        rodbus::client::spawn_tcp_client_task(
+                   addr,
+                   max_queued_requests,
+             rodbus::client::channel::strategy::default()
+        )
+    );
 
     Box::into_raw(boxed)
 }
 
 #[no_mangle]
-pub extern "C" fn destroy_tcp_client(channel: *mut rodbus::client::channel::Channel) {
-    if channel != null_mut() {
-        unsafe { Box::from_raw(channel) };
+pub extern "C" fn destroy_tcp_client(client: *mut rodbus::client::channel::Channel) {
+    if client != null_mut() {
+        unsafe { Box::from_raw(client) };
     };
     ()
 }
