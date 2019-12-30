a98cbe07c7ef703ac17ddb8659147c199d4fe32c
Émile Grégoire <eg@emilegregoire.ca>
2019-12-30 13:22:36 -0500

    Moved examples and isolate rodbus-client.

---
+++ rodbus/examples/client.rs
@@ -0,0 +1,21 @@
+use std::time::Duration;
+
+use rodbus::prelude::*;
+use std::error::Error;
+
+#[tokio::main(basic_scheduler)]
+async fn main() -> Result<(), Box<dyn Error>> {
+
+    // Create a channel
+    let channel = spawn_tcp_client_task("127.0.0.1:502".parse().unwrap(), 1, strategy::default());
+
+    // Create a session
+    let mut session = channel.create_session(UnitId::new(1), Duration::from_secs(1));
+
+    // Send request
+    for x in session.read_coils(AddressRange::new(0, 10)).await? {
+        println!("index: {} value: {}", x.index, x.value);
+    }
+
+    Ok(())
+}
