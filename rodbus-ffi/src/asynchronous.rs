c35fe980b167597cdf417c010c172f6faa99acaf
jadamcrain <jadamcrain@gmail.com>
2019-12-31 14:05:26 -0800

    remove code duplication in ffi async callbacks

--- rodbus-ffi/src/asynchronous.rs
+++ rodbus-ffi/src/asynchronous.rs
@@ -1,25 +1,30 @@
 use super::*;
+use rodbus::types::Indexed;
 
-#[no_mangle]
-pub unsafe extern "C" fn read_coils_cb(
+unsafe fn get_callback_session<'a>(
     session: *mut Session,
-    start: u16,
-    count: u16,
-    callback: Option<unsafe extern "C" fn(Result, *const bool, usize, *mut c_void)>,
-    context: *mut c_void,
-) {
+) -> (&'a mut tokio::runtime::Runtime, CallbackSession) {
     let s = session.as_mut().unwrap();
     let runtime = s.runtime.as_mut().unwrap();
     let channel = s.channel.as_mut().unwrap();
 
-    let mut session: CallbackSession = CallbackSession::new(channel.create_session(
+    let session = CallbackSession::new(channel.create_session(
         UnitId::new(s.unit_id),
         std::time::Duration::from_millis(s.timeout_ms as u64),
     ));
 
-    let storage = ContextStorage { context };
+    (runtime, session)
+}
 
-    session.read_coils(runtime, AddressRange::new(start, count), move |result| {
+type BoolResult = std::result::Result<Vec<Indexed<bool>>, rodbus::error::Error>;
+type U16Result = std::result::Result<Vec<Indexed<u16>>, rodbus::error::Error>;
+
+unsafe fn bool_callback_to_fn(
+    context: *mut c_void,
+    callback: Option<unsafe extern "C" fn(Result, *const bool, usize, *mut c_void)>,
+) -> impl Fn(BoolResult) -> () {
+    let storage = ContextStorage { context };
+    move |result| {
         if let Some(cb) = callback {
             match result {
                 Err(err) => cb(err.kind().into(), null(), 0, storage.context),
@@ -34,7 +39,46 @@ pub unsafe extern "C" fn read_coils_cb(
                 }
             }
         }
-    });
+    }
+}
+
+unsafe fn u16_callback_to_fn(
+    context: *mut c_void,
+    callback: Option<unsafe extern "C" fn(Result, *const u16, usize, *mut c_void)>,
+) -> impl Fn(U16Result) -> () {
+    let storage = ContextStorage { context };
+    move |result| {
+        if let Some(cb) = callback {
+            match result {
+                Err(err) => cb(err.kind().into(), null(), 0, storage.context),
+                Ok(values) => {
+                    let transformed: Vec<u16> = values.iter().map(|x| x.value).collect();
+                    cb(
+                        Result::status(Status::Ok),
+                        transformed.as_ptr(),
+                        transformed.len(),
+                        storage.context,
+                    )
+                }
+            }
+        }
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn read_coils_cb(
+    session: *mut Session,
+    start: u16,
+    count: u16,
+    callback: Option<unsafe extern "C" fn(Result, *const bool, usize, *mut c_void)>,
+    context: *mut c_void,
+) {
+    let (runtime, mut session) = get_callback_session(session);
+    session.read_coils(
+        runtime,
+        AddressRange::new(start, count),
+        bool_callback_to_fn(context, callback),
+    );
 }
 
 #[no_mangle]
@@ -45,31 +89,42 @@ pub unsafe extern "C" fn read_discrete_inputs_cb(
     callback: Option<unsafe extern "C" fn(Result, *const bool, usize, *mut c_void)>,
     context: *mut c_void,
 ) {
-    let s = session.as_mut().unwrap();
-    let runtime = s.runtime.as_mut().unwrap();
-    let channel = s.channel.as_mut().unwrap();
+    let (runtime, mut session) = get_callback_session(session);
+    session.read_discrete_inputs(
+        runtime,
+        AddressRange::new(start, count),
+        bool_callback_to_fn(context, callback),
+    );
+}
 
-    let mut session: CallbackSession = CallbackSession::new(channel.create_session(
-        UnitId::new(s.unit_id),
-        std::time::Duration::from_millis(s.timeout_ms as u64),
-    ));
+#[no_mangle]
+pub unsafe extern "C" fn read_holding_registers_cb(
+    session: *mut Session,
+    start: u16,
+    count: u16,
+    callback: Option<unsafe extern "C" fn(Result, *const u16, usize, *mut c_void)>,
+    context: *mut c_void,
+) {
+    let (runtime, mut session) = get_callback_session(session);
+    session.read_holding_registers(
+        runtime,
+        AddressRange::new(start, count),
+        u16_callback_to_fn(context, callback),
+    );
+}
 
-    let storage = ContextStorage { context };
-
-    session.read_discrete_inputs(runtime, AddressRange::new(start, count), move |result| {
-        if let Some(cb) = callback {
-            match result {
-                Err(err) => cb(err.kind().into(), null(), 0, storage.context),
-                Ok(values) => {
-                    let transformed: Vec<bool> = values.iter().map(|x| x.value).collect();
-                    cb(
-                        Result::status(Status::Ok),
-                        transformed.as_ptr(),
-                        transformed.len(),
-                        storage.context,
-                    )
-                }
-            }
-        }
-    });
+#[no_mangle]
+pub unsafe extern "C" fn read_input_registers_cb(
+    session: *mut Session,
+    start: u16,
+    count: u16,
+    callback: Option<unsafe extern "C" fn(Result, *const u16, usize, *mut c_void)>,
+    context: *mut c_void,
+) {
+    let (runtime, mut session) = get_callback_session(session);
+    session.read_input_registers(
+        runtime,
+        AddressRange::new(start, count),
+        u16_callback_to_fn(context, callback),
+    );
 }
