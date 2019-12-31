beb2d6d73211c0b626774f37090830e66c1bd49b
jadamcrain <jadamcrain@gmail.com>
2019-12-31 12:28:53 -0800

    add functions. now to consider code duplication in ffi...

--- rodbus-ffi/src/synchronous.rs
+++ rodbus-ffi/src/synchronous.rs
@@ -17,8 +17,34 @@ pub unsafe extern "C" fn read_coils(
     ));
     match session.read_coils(runtime, AddressRange::new(start, count)) {
         Ok(coils) => {
-            for (i, coil) in coils.iter().enumerate() {
-                *output.add(i) = coil.value
+            for (i, indexed) in coils.iter().enumerate() {
+                *output.add(i) = indexed.value
+            }
+            Result::status(Status::Ok)
+        }
+        Err(e) => e.kind().into(),
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn read_discrete_inputs(
+    session: *mut Session,
+    start: u16,
+    count: u16,
+    output: *mut bool,
+) -> Result {
+    let s = session.as_mut().unwrap();
+    let runtime = s.runtime.as_mut().unwrap();
+    let channel = s.channel.as_mut().unwrap();
+
+    let mut session: SyncSession = SyncSession::new(channel.create_session(
+        UnitId::new(s.unit_id),
+        std::time::Duration::from_millis(s.timeout_ms as u64),
+    ));
+    match session.read_discrete_inputs(runtime, AddressRange::new(start, count)) {
+        Ok(coils) => {
+            for (i, indexed) in coils.iter().enumerate() {
+                *output.add(i) = indexed.value
             }
             Result::status(Status::Ok)
         }
