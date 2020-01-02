935228ddd3fced5e20bdc5bdcffaad98c76c87b3
jadamcrain <jadamcrain@gmail.com>
2020-01-01 18:20:21 -0800

    set the max log level

--- rodbus-ffi/src/logging.rs
+++ rodbus-ffi/src/logging.rs
@@ -5,7 +5,7 @@ use std::ffi::CString;
 #[repr(u8)]
 pub enum Level {
     Error,
-    Warning,
+    Warn,
     Info,
     Debug,
     Trace,
@@ -17,7 +17,8 @@ pub enum Level {
 ///
 /// @return true if the callback was successfully set
 /// @warning this call will only succeed the first time it is made!
-pub fn set_log_callback(
+#[no_mangle]
+pub extern "C" fn set_log_callback(
     callback: Option<
         unsafe extern "C" fn(level: Level, message: *const std::os::raw::c_char) -> (),
     >,
@@ -28,15 +29,33 @@ pub fn set_log_callback(
     }
 }
 
+/// @brief set the maximum log level
+///
+/// @param level maximum level at which messages will be logged
+#[no_mangle]
+pub extern "C" fn set_max_level(level: Level) {
+    log::set_max_level(to_filter(level));
+}
+
 impl std::convert::From<log::Level> for Level {
     fn from(level: log::Level) -> Self {
         match level {
             log::Level::Error => Level::Error,
-            log::Level::Warn => Level::Warning,
+            log::Level::Warn => Level::Warn,
             log::Level::Info => Level::Info,
             log::Level::Debug => Level::Debug,
             log::Level::Trace => Level::Trace,
         }
+    }
+}
+
+fn to_filter(level: Level) -> log::LevelFilter {
+    match level {
+        Level::Error => log::LevelFilter::Error,
+        Level::Warn => log::LevelFilter::Warn,
+        Level::Info => log::LevelFilter::Info,
+        Level::Debug => log::LevelFilter::Debug,
+        Level::Trace => log::LevelFilter::Trace,
     }
 }
 
