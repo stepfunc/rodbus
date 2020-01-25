2643e6ff1b4ef4e39a6b6447fd9fde49f7a298c8
jadamcrain <jadamcrain@gmail.com>
2020-01-24 18:39:16 -0800

    collapse the writes into one generic impl

--- rodbus-ffi/src/server.rs
+++ rodbus-ffi/src/server.rs
@@ -2,11 +2,11 @@ use crate::user_data::UserData;
 use rodbus::error::details::ExceptionCode;
 use rodbus::server::handler::ServerHandler;
 use rodbus::types::{AddressRange, Indexed};
+use std::ops::DerefMut;
 use std::os::raw::c_void;
 
 // if these return true, we update the underlying value automatically
-type WriteSingleCoilCB = Option<unsafe extern "C" fn(bool, u16, *mut c_void) -> bool>;
-type WriteSingleRegisterCB = Option<unsafe extern "C" fn(u16, u16, *mut c_void) -> bool>;
+type WriteSingleCallback<T> = Option<unsafe extern "C" fn(T, u16, *mut c_void) -> bool>;
 
 struct Data {
     bo: Vec<bool>,
@@ -17,8 +17,8 @@ struct Data {
 
 struct Callbacks {
     user_data: UserData,
-    write_single_coil_cb: WriteSingleCoilCB,
-    write_single_register_cb: WriteSingleRegisterCB,
+    write_single_coil_cb: WriteSingleCallback<bool>,
+    write_single_register_cb: WriteSingleCallback<u16>,
 }
 
 struct FFIHandler {
@@ -40,8 +40,8 @@ impl Data {
 impl Callbacks {
     pub(crate) fn new(
         user_data: *mut c_void,
-        write_single_coil_cb: WriteSingleCoilCB,
-        write_single_register_cb: WriteSingleRegisterCB,
+        write_single_coil_cb: WriteSingleCallback<bool>,
+        write_single_register_cb: WriteSingleCallback<u16>,
     ) -> Self {
         Self {
             user_data: UserData::new(user_data),
@@ -54,6 +54,29 @@ impl Callbacks {
 impl FFIHandler {
     pub fn new(data: Data, callbacks: Callbacks) -> Self {
         Self { data, callbacks }
+    }
+
+    fn write_single<T>(
+        pair: Indexed<T>,
+        vec: &mut Vec<T>,
+        callback: WriteSingleCallback<T>,
+        user_data: &mut UserData,
+    ) -> Result<(), ExceptionCode>
+    where
+        T: Copy,
+    {
+        match callback {
+            Some(func) => match vec.get_mut(pair.index as usize) {
+                Some(value) => unsafe {
+                    if func(pair.value, pair.index, user_data.value) {
+                        *value = pair.value
+                    }
+                    Ok(())
+                },
+                None => Err(ExceptionCode::IllegalDataValue),
+            },
+            None => Err(ExceptionCode::IllegalFunction),
+        }
     }
 }
 
@@ -75,32 +98,20 @@ impl ServerHandler for FFIHandler {
     }
 
     fn write_single_coil(&mut self, pair: Indexed<bool>) -> Result<(), ExceptionCode> {
-        match self.callbacks.write_single_coil_cb {
-            Some(func) => match self.data.bo.get_mut(pair.index as usize) {
-                Some(value) => unsafe {
-                    if func(pair.value, pair.index, self.callbacks.user_data.value) {
-                        *value = pair.value
-                    }
-                    Ok(())
-                },
-                None => Err(ExceptionCode::IllegalDataValue),
-            },
-            None => Err(ExceptionCode::IllegalFunction),
-        }
+        Self::write_single(
+            pair,
+            &mut self.data.bo,
+            self.callbacks.write_single_coil_cb,
+            &mut self.callbacks.user_data,
+        )
     }
 
     fn write_single_register(&mut self, pair: Indexed<u16>) -> Result<(), ExceptionCode> {
-        match self.callbacks.write_single_register_cb {
-            Some(func) => match self.data.ao.get_mut(pair.index as usize) {
-                Some(value) => unsafe {
-                    if func(pair.value, pair.index, self.callbacks.user_data.value) {
-                        *value = pair.value
-                    }
-                    Ok(())
-                },
-                None => Err(ExceptionCode::IllegalDataValue),
-            },
-            None => Err(ExceptionCode::IllegalFunction),
-        }
+        Self::write_single(
+            pair,
+            &mut self.data.ao,
+            self.callbacks.write_single_register_cb,
+            &mut self.callbacks.user_data,
+        )
     }
 }
