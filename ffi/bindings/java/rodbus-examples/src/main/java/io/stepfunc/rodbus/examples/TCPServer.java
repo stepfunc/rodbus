333691ab2eff8ca855965a424d251d772aa4e6ec
jadamcrain <jadamcrain@gmail.com>
2020-09-21 11:57:27 -0700

    use helper methods for WriteResult in Java

--- ffi/bindings/java/rodbus-examples/src/main/java/io/stepfunc/rodbus/examples/TCPServer.java
+++ ffi/bindings/java/rodbus-examples/src/main/java/io/stepfunc/rodbus/examples/TCPServer.java
@@ -1,13 +1,14 @@
 package io.stepfunc.rodbus.examples;
 
-import io.stepfunc.rodbus.*;
 import io.stepfunc.rodbus.Exception;
 import io.stepfunc.rodbus.Runtime;
+import io.stepfunc.rodbus.*;
 import org.joou.UShort;
 
 import java.util.Collection;
 
-import static org.joou.Unsigned.*;
+import static org.joou.Unsigned.ubyte;
+import static org.joou.Unsigned.ushort;
 
 public class TCPServer {
 
@@ -18,61 +19,43 @@ public class TCPServer {
         }
     }
 
-    static WriteResult failure(Exception ex)
-    {
-        WriteResult res = new WriteResult();
-        res.success = false;
-        res.exception = ex;
-        res.rawException = ubyte(0);
-        return res;
-    }
-
-    static WriteResult success()
-    {
-        WriteResult res = new WriteResult();
-        res.success = true;
-        res.exception = Exception.UNKNOWN;
-        res.rawException = ubyte(0);
-        return res;
-    }
-
     static class SimpleWriteHandler implements WriteHandler {
         @Override
         public WriteResult writeSingleCoil(Boolean value, UShort index, Database database) {
-            if(database.updateCoil(index, value)) {
-                return success();
+            if (database.updateCoil(index, value)) {
+                return WriteResult.createSuccess();
             } else {
-                return failure(Exception.ILLEGAL_DATA_ADDRESS);
+                return WriteResult.createException(Exception.ILLEGAL_DATA_ADDRESS);
             }
         }
 
         @Override
         public WriteResult writeSingleRegister(UShort value, UShort index, Database database) {
-            if(database.updateHoldingRegister(index, value)) {
-                return success();
+            if (database.updateHoldingRegister(index, value)) {
+                return WriteResult.createSuccess();
             } else {
-                return failure(Exception.ILLEGAL_DATA_ADDRESS);
+                return WriteResult.createException(Exception.ILLEGAL_DATA_ADDRESS);
             }
         }
 
         @Override
         public WriteResult writeMultipleCoils(UShort start, Collection<Bit> it, Database database) {
-           for(Bit bit : it) {
-               if(!database.updateCoil(bit.index, bit.value)) {
-                   return failure(Exception.ILLEGAL_DATA_ADDRESS);
-               }
-           }
-           return success();
+            for (Bit bit : it) {
+                if (!database.updateCoil(bit.index, bit.value)) {
+                    return WriteResult.createException(Exception.ILLEGAL_DATA_ADDRESS);
+                }
+            }
+            return WriteResult.createSuccess();
         }
 
         @Override
         public WriteResult writeMultipleRegisters(UShort start, Collection<Register> it, Database database) {
-            for(Register reg : it) {
-                if(!database.updateHoldingRegister(reg.index, reg.value)) {
-                    return failure(Exception.ILLEGAL_DATA_ADDRESS);
+            for (Register reg : it) {
+                if (!database.updateHoldingRegister(reg.index, reg.value)) {
+                    return WriteResult.createException(Exception.ILLEGAL_DATA_ADDRESS);
                 }
             }
-            return success();
+            return WriteResult.createSuccess();
         }
     }
 
@@ -85,7 +68,7 @@ public class TCPServer {
     static DeviceMap buildDeviceMap() {
         DeviceMap map = new DeviceMap();
         map.addEndpoint(ubyte(1), new SimpleWriteHandler(), (Database db) -> {
-            for(int i = 0; i < 10; ++i) {
+            for (int i = 0; i < 10; ++i) {
                 db.addCoil(ushort(i), false);
                 db.addDiscreteInput(ushort(i), false);
                 db.addHoldingRegister(ushort(i), ushort(0));
@@ -101,7 +84,7 @@ public class TCPServer {
 
         void next() {
             this.registerValue += 1;
-            if(this.registerValue > UShort.MAX_VALUE) {
+            if (this.registerValue > UShort.MAX_VALUE) {
                 this.registerValue = 0;
             }
             this.bitValue = !this.bitValue;
@@ -121,10 +104,10 @@ public class TCPServer {
 
         final State state = new State();
 
-        while(true) {
+        while (true) {
             server.update(ubyte(1), (Database db) -> {
                 state.next();
-                for(int i = 0; i < 10; ++i) {
+                for (int i = 0; i < 10; ++i) {
                     db.updateInputRegister(ushort(i), state.getRegisterValue());
                     db.updateDiscreteInput(ushort(i), state.getBitValue());
                 }
@@ -137,7 +120,7 @@ public class TCPServer {
         Logging.setMaxLogLevel(LogLevel.INFO);
         Logging.setHandler(new ConsoleLogger());
 
-        try(final Runtime runtime = createRuntime()) {
+        try (final Runtime runtime = createRuntime()) {
             run(runtime);
         }
     }
