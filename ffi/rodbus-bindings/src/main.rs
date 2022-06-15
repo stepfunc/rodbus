64d082298205041ac86b985664391eb0378f002f
Adam Crain <jadamcrain@gmail.com>
2022-06-15 15:23:16 -0700

    Refactor/new jni gen (#69)
    * move to oo_bindgen 0.2.1 and generate JNI as part of workspace
    
    * first pass at new CI
    
    * add additional clippy exclusion
    
    * build and upload Java bindings
    
    * fix typo

--- ffi/rodbus-bindings/src/main.rs
+++ ffi/rodbus-bindings/src/main.rs
@@ -1,13 +1,16 @@
-use std::path::Path;
+use std::path::PathBuf;
+use std::rc::Rc;
 
 pub fn main() {
+    let library = rodbus_schema::build_lib().unwrap();
+
     let builder_settings = ci_script::BindingBuilderSettings {
         ffi_target_name: "rodbus-ffi",
         ffi_name: "rodbus_ffi",
-        ffi_path: Path::new("ffi/rodbus-ffi"),
+        ffi_path: PathBuf::from("ffi/rodbus-ffi"),
         java_group_id: "io.stepfunc",
-        destination_path: Path::new("ffi/bindings"),
-        library: &rodbus_schema::build_lib().unwrap(),
+        destination_path: PathBuf::from("ffi/bindings"),
+        library: Rc::new(library),
     };
 
     ci_script::run(builder_settings);
