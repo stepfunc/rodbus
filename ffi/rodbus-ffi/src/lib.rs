0b769386ae7edb46007e970b1c0fcdc1ab279c2e
jadamcrain <jadamcrain@gmail.com>
2022-09-30 10:02:57 -0700

    apply the tokio-ffi-schema crate

--- ffi/rodbus-ffi/src/lib.rs
+++ ffi/rodbus-ffi/src/lib.rs
@@ -27,16 +27,41 @@ pub use server::*;
 
 pub mod ffi;
 
-impl From<crate::TracingInitError> for std::os::raw::c_int {
-    fn from(_: crate::TracingInitError) -> Self {
-        crate::ffi::ParamError::LoggingAlreadyConfigured.into()
-    }
-}
-
 lazy_static::lazy_static! {
     static ref VERSION: std::ffi::CString = std::ffi::CString::new(rodbus::VERSION).unwrap();
 }
 
 fn version() -> &'static std::ffi::CStr {
     &VERSION
+}
+
+// the From<> impls below are needed to map tracing and tokio ffi stuff to the actual errors used in this crate
+
+impl From<crate::TracingInitError> for std::os::raw::c_int {
+    fn from(_: crate::TracingInitError) -> Self {
+        crate::ffi::ParamError::LoggingAlreadyConfigured.into()
+    }
+}
+
+impl From<crate::runtime::RuntimeError> for crate::ffi::ParamError {
+    fn from(err: crate::runtime::RuntimeError) -> Self {
+        match err {
+            crate::runtime::RuntimeError::RuntimeDestroyed => {
+                crate::ffi::ParamError::RuntimeDestroyed
+            }
+            crate::runtime::RuntimeError::CannotBlockWithinAsync => {
+                crate::ffi::ParamError::RuntimeCannotBlockWithinAsync
+            }
+            crate::runtime::RuntimeError::FailedToCreateRuntime => {
+                crate::ffi::ParamError::RuntimeCreationFailure
+            }
+        }
+    }
+}
+
+impl From<crate::runtime::RuntimeError> for std::os::raw::c_int {
+    fn from(err: crate::runtime::RuntimeError) -> Self {
+        let err: crate::ffi::ParamError = err.into();
+        err.into()
+    }
 }
