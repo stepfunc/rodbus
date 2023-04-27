a5cc6ad5d25a65f246f6a9de7416903907e2d20d
Adam Crain <jadamcrain@gmail.com>
2023-04-27 12:57:39 -0700

    Ability to disable server name checking in TLS client (#118)
    * Add self_signed and full_pki constructors to the TLS ClientConfig and deprecate legacy new
    
    * Add the ability to disable server name checking in the TLS client
    
    * fix feature builds

--- rodbus/src/tcp/tls/client.rs
+++ rodbus/src/tcp/tls/client.rs
@@ -1,4 +1,5 @@
 use std::convert::TryFrom;
+use std::net::Ipv4Addr;
 
 use sfio_rustls_config::NameVerifier;
 use std::path::Path;
@@ -17,7 +18,7 @@ use crate::DecodeLevel;
 
 /// TLS configuration
 pub struct TlsClientConfig {
-    dns_name: rustls::ServerName,
+    server_name: rustls::ServerName,
     config: Arc<rustls::ClientConfig>,
 }
 
@@ -67,9 +68,13 @@ pub(crate) fn create_tls_channel(
 }
 
 impl TlsClientConfig {
-    /// Create a TLS master config
+    /// Legacy method for creating a client TLS configuration
+    #[deprecated(
+        since = "1.3.0",
+        note = "Please use `full_pki` or `self_signed` instead"
+    )]
     pub fn new(
-        name: &str,
+        server_name: &str,
         peer_cert_path: &Path,
         local_cert_path: &Path,
         private_key_path: &Path,
@@ -77,29 +82,94 @@ impl TlsClientConfig {
         min_tls_version: MinTlsVersion,
         certificate_mode: CertificateMode,
     ) -> Result<Self, TlsError> {
-        let dns_name = rustls::ServerName::try_from(name).map_err(|_| TlsError::InvalidDnsName)?;
+        match certificate_mode {
+            CertificateMode::AuthorityBased => Self::full_pki(
+                Some(server_name.to_string()),
+                peer_cert_path,
+                local_cert_path,
+                private_key_path,
+                password,
+                min_tls_version,
+            ),
+            CertificateMode::SelfSigned => Self::self_signed(
+                peer_cert_path,
+                local_cert_path,
+                private_key_path,
+                password,
+                min_tls_version,
+            ),
+        }
+    }
 
-        let config = match certificate_mode {
-            CertificateMode::SelfSigned => sfio_rustls_config::client::self_signed(
-                min_tls_version.into(),
-                peer_cert_path,
-                local_cert_path,
-                private_key_path,
-                password,
-            )?,
-            CertificateMode::AuthorityBased => sfio_rustls_config::client::authority(
-                min_tls_version.into(),
-                NameVerifier::equal_to(name.to_string()),
-                peer_cert_path,
-                local_cert_path,
-                private_key_path,
-                password,
-            )?,
+    /// Create a TLS client configuration that expects a full PKI with an authority, and possibly
+    /// intermediate CA certificates.
+    ///
+    /// If `server_subject_name` is specified, than the client will verify that the name is present in the
+    /// SAN extension or in the Common Name of the client certificate.
+    ///
+    /// If `server_subject_name` is set to None, then no server name validation is performed, and
+    /// any authenticated server is allowed.
+    pub fn full_pki(
+        server_subject_name: Option<String>,
+        peer_cert_path: &Path,
+        local_cert_path: &Path,
+        private_key_path: &Path,
+        password: Option<&str>,
+        min_tls_version: MinTlsVersion,
+    ) -> Result<Self, TlsError> {
+        let (name_verifier, server_name) = match server_subject_name {
+            None => (
+                NameVerifier::any(),
+                rustls::ServerName::IpAddress(Ipv4Addr::UNSPECIFIED.into()),
+            ),
+            Some(x) => {
+                let server_name = rustls::ServerName::try_from(x.as_str())?;
+                (NameVerifier::equal_to(x), server_name)
+            }
         };
 
+        let config = sfio_rustls_config::client::authority(
+            min_tls_version.into(),
+            name_verifier,
+            peer_cert_path,
+            local_cert_path,
+            private_key_path,
+            password,
+        )?;
+
         Ok(Self {
+            server_name,
             config: Arc::new(config),
-            dns_name,
+        })
+    }
+
+    /// Create a TLS client configuration that expects the client to present a single certificate.
+    ///
+    /// In lieu of performing server subject name validation, the client validates:
+    ///
+    /// 1) That the server presents a single certificate
+    /// 2) That the certificate is a byte-for-byte match with the one loaded in `peer_cert_path`.
+    /// 3) That the certificate's Validity (not before / not after) is currently valid.
+    ///
+    pub fn self_signed(
+        peer_cert_path: &Path,
+        local_cert_path: &Path,
+        private_key_path: &Path,
+        password: Option<&str>,
+        min_tls_version: MinTlsVersion,
+    ) -> Result<Self, TlsError> {
+        let config = sfio_rustls_config::client::self_signed(
+            min_tls_version.into(),
+            peer_cert_path,
+            local_cert_path,
+            private_key_path,
+            password,
+        )?;
+
+        Ok(Self {
+            //  it doesn't matter what we put here, it just needs to be an IP so that the client won't send an SNI extension
+            server_name: rustls::ServerName::IpAddress(Ipv4Addr::UNSPECIFIED.into()),
+            config: Arc::new(config),
         })
     }
 
@@ -109,7 +179,7 @@ impl TlsClientConfig {
         endpoint: &HostAddr,
     ) -> Result<PhysLayer, String> {
         let connector = tokio_rustls::TlsConnector::from(self.config.clone());
-        match connector.connect(self.dns_name.clone(), socket).await {
+        match connector.connect(self.server_name.clone(), socket).await {
             Err(err) => Err(format!(
                 "failed to establish TLS session with {endpoint}: {err}"
             )),
