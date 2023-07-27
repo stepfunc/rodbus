221519fbba46df739913f9a30686d33a52d3af28
Kay Grewe <Kay.Grewe@protonmail.com>
2023-07-27 12:55:18 +0200

    refactor(read_device_ident): refactor, types renamed, println removed

--- rodbus/src/client/requests/read_device_identification.rs
+++ rodbus/src/client/requests/read_device_identification.rs
@@ -1,19 +1,19 @@
 use scursor::{WriteCursor, ReadCursor};
 
-use crate::{RequestError, DeviceIdentification, ReadDeviceInfoBlock, common::{traits::Serialize, function::FunctionCode}, AppDecodeLevel};
+use crate::{RequestError, DeviceInfo, ReadDeviceRequest, common::{traits::Serialize, function::FunctionCode}, AppDecodeLevel};
 
 
-pub(crate) struct ReadDeviceIdentification {
-    pub(crate) request: ReadDeviceInfoBlock,
+pub(crate) struct ReadDevice {
+    pub(crate) request: ReadDeviceRequest,
     promise: Promise,
 }
 
 pub(crate) trait DeviceIdentificationCallback:
-    FnOnce(Result<DeviceIdentification, RequestError>) + Send + Sync + 'static  {
+    FnOnce(Result<DeviceInfo, RequestError>) + Send + Sync + 'static  {
 }
 
 impl<T> DeviceIdentificationCallback for T where 
-    T: FnOnce(Result<DeviceIdentification, RequestError>) + Send + Sync + 'static {
+    T: FnOnce(Result<DeviceInfo, RequestError>) + Send + Sync + 'static {
 
 }
 
@@ -38,34 +38,39 @@ impl Promise {
         self.complete(Err(err));
     }
 
-    pub(crate) fn success(&mut self, identifier: DeviceIdentification) {
+    pub(crate) fn success(&mut self, identifier: DeviceInfo) {
         self.complete(Ok(identifier));
     }
 
-    fn complete(&mut self, x: Result<DeviceIdentification, RequestError>) {
+    fn complete(&mut self, x: Result<DeviceInfo, RequestError>) {
         if let Some(callback) = self.callback.take() {
             callback(x);
         }
     }
 }
 
-impl ReadDeviceIdentification {
-    fn new(request: ReadDeviceInfoBlock, promise: Promise) -> Self {
+impl ReadDevice {
+    fn new(request: ReadDeviceRequest, promise: Promise) -> Self {
         Self {
             request,
             promise,
         }
     }
 
-    pub(crate) fn channel(request: ReadDeviceInfoBlock,
-        tx: tokio::sync::oneshot::Sender<Result<DeviceIdentification, RequestError>>) -> Self {
-            Self::new(request, Promise::new(|x: Result<DeviceIdentification, RequestError>| {
-                let _ = tx.send(x);
-            }))
-    }
-
     pub(crate) fn serialize(&self, cursor: &mut WriteCursor) -> Result<(), RequestError> {
         self.request.serialize(cursor)
+    }
+
+    pub(crate) fn channel(
+        request: ReadDeviceRequest,
+        tx: tokio::sync::oneshot::Sender<Result<DeviceInfo, RequestError>>,
+    ) -> Self {
+        Self::new(
+            request,
+            Promise::new(|x: Result<DeviceInfo, RequestError>| {
+                let _ = tx.send(x);
+            }),
+        )
     }
 
     pub(crate) fn failure(&mut self, err: RequestError) {
@@ -79,7 +84,7 @@ impl ReadDeviceIdentification {
         decode: AppDecodeLevel,
     ) -> Result<(), RequestError> {
         println!("{}", self.request);
-        let response = Self::parse_device_identification_response(self.request, &mut cursor)?;
+        let response = Self::parse_device_identification_response(&mut cursor)?;
 
         if decode.enabled() {
             tracing::info!(
@@ -94,34 +99,26 @@ impl ReadDeviceIdentification {
     }
 
     fn parse_device_identification_response<'a>(
-        request: ReadDeviceInfoBlock,
         cursor: &'a mut ReadCursor,
-    ) -> Result<DeviceIdentification, RequestError> {        
+    ) -> Result<DeviceInfo, RequestError> {        
         let mei_code = cursor.read_u8()?;
-        println!("mei_code: {}", mei_code);
-
         let device_id = cursor.read_u8()?;
-        println!("device_id: {}", device_id);
-
         let conformity_level = cursor.read_u8()?;
-        println!("conformity_level: {}", conformity_level);
 
         let more_follows = cursor.read_u8()?;
         let continue_at = cursor.read_u8()?;
-        println!("more_follows {}", more_follows);
-        println!("continue_at {}", continue_at);
 
 
         //TODO(Kay): Currently this read can be off by one element we should make sure that it doesn't miss a value !
         let msglength = cursor.read_u8()?;
-        println!("got {} objects", msglength);
         
-        let mut result = DeviceIdentification::new(mei_code, device_id, conformity_level);
+        let mut result = DeviceInfo::new(mei_code, device_id, conformity_level);
+        
         if more_follows == 0xFF {
             result.continue_at = Some(continue_at);
         }
 
-        ReadDeviceIdentification::parse_device_info_objects(msglength, &mut result.storage, cursor)?;
+        ReadDevice::parse_device_info_objects(msglength, &mut result.storage, cursor)?;
         
         Ok(result)
     }
@@ -131,19 +128,19 @@ impl ReadDeviceIdentification {
             //TODO(Kay): Do we need to store the obj_id ? 
             let _obj_id = cursor.read_u8()?;
             let str_size = cursor.read_u8()?;
-            println!("Reading a string with size {} from the input stream", str_size as usize);
             
             let data = cursor.read_bytes(str_size as usize)?.to_vec();
-            println!("Raw data read: {:X?}", data);
 
             let str = String::from_utf8(data);
 
             match str {
                 Ok(str) => {
-                    container.push(str)
+                    match str.is_ascii() {
+                        true => container.push(str),
+                        false => return Err(RequestError::Io(std::io::ErrorKind::InvalidData)),
+                    }
                 }
-                Err(e) => {
-                    //TODO(Kay): Figure out what todo if the device information does not contain valid utf-8 or ascii data ?
+                Err(_) => {
                     return Err(RequestError::Io(std::io::ErrorKind::InvalidData))
                 },
             }
