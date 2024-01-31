6c8ac0a509f8e81c1b1128bd34c2045346248740
dkoehler-dev <admin@dominikkoehler.dev>
2024-01-31 17:56:38 +0100

    fix(read_write_multiple): fix read_write_multiple client request types

--- rodbus/src/client/requests/read_write_multiple.rs
+++ rodbus/src/client/requests/read_write_multiple.rs
@@ -1,24 +1,91 @@
 use crate::client::message::Promise;
 use crate::common::function::FunctionCode;
 use crate::common::traits::{Parse, Serialize};
-use crate::types::AddressRange;
 use crate::decode::AppDecodeLevel;
-use crate::error::{InvalidRequest, RequestError, AduParseError};
+use crate::error::RequestError;
+use crate::error::{AduParseError, InvalidRequest};
+use crate::types::{AddressRange, Indexed};
 
 use scursor::{ReadCursor, WriteCursor};
+use std::convert::TryFrom;
 
+/// Collection of values and starting address
+///
+/// Used when making write multiple coil/register requests
 #[derive(Debug, Clone)]
 pub struct ReadWriteMultiple<T> {
+    /// starting address
     pub(crate) read_range: AddressRange,
+    /// starting address
     pub(crate) write_range: AddressRange,
-    pub(crate) write_values: Vec<T>,
+    /// vector of values
+    pub(crate) values: Vec<T>,
+}
+
+pub(crate) struct ReadWriteMultipleIterator<'a, T> {
+    range: AddressRange,
+    pos: u16,
+    iter: std::slice::Iter<'a, T>,
 }
 
 impl<T> ReadWriteMultiple<T> {
-    pub fn new(read_start: u16, read_count: u16, write_start: u16, write_count: u16, write_values: Vec<T>) -> Result<Self, InvalidRequest> {
-        let read_range = AddressRange::try_from(read_start, read_count)?;
-        let write_range = AddressRange::try_from(write_start, write_count)?;
-        Ok(Self { read_range, write_range, write_values })
+    /// Create new collection of values
+    pub fn new(
+        read_start: u16,
+        write_start: u16,
+        values: Vec<T>,
+    ) -> Result<Self, InvalidRequest> {
+        let count = match u16::try_from(values.len()) {
+            Ok(x) => x,
+            Err(_) => return Err(InvalidRequest::CountTooBigForU16(values.len())),
+        };
+        let read_range = AddressRange::try_from(read_start, count)?;
+        let write_range = AddressRange::try_from(write_start, count)?;
+        Ok(Self {
+            read_range,
+            write_range,
+            values,
+        })
+    }
+
+    pub(crate) fn iter(&self) -> ReadWriteMultipleIterator<'_, T> {
+        ReadWriteMultipleIterator::new(self.read_range, self.values.iter())
+    }
+}
+
+impl<'a, T> ReadWriteMultipleIterator<'a, T> {
+    fn new(range: AddressRange, iter: std::slice::Iter<'a, T>) -> Self {
+        Self {
+            range,
+            pos: 0,
+            iter,
+        }
+    }
+}
+
+impl<T> Iterator for ReadWriteMultipleIterator<'_, T>
+where
+    T: Copy,
+{
+    type Item = Indexed<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let next = self.iter.next();
+
+        match next {
+            Some(next) => {
+                let result = Indexed::new(self.range.start + self.pos, *next);
+                self.pos += 1;
+                Some(result)
+            }
+            None => None,
+        }
+    }
+
+    // implementing this allows collect to optimize the vector capacity
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let remaining = (self.range.count - self.pos) as usize;
+        (remaining, Some(remaining))
     }
 }
 
@@ -38,18 +105,22 @@ where
         Self { request, promise }
     }
 
-    pub(crate) fn serialize(self, cursor: &mut WriteCursor) -> Result<(), RequestError> {
-        self.request.serialize(cursor)?;
-        Ok(())
+    pub(crate) fn serialize(&self, cursor: &mut WriteCursor) -> Result<(), RequestError> {
+        self.request.serialize(cursor)
     }
 
     pub(crate) fn failure(&mut self, err: RequestError) {
         self.promise.failure(err)
     }
 
-    pub(crate) fn handle_response(&mut self, cursor: ReadCursor, function: FunctionCode, decode: AppDecodeLevel) -> Result<(), RequestError> {
+    pub(crate) fn handle_response(
+        &mut self,
+        cursor: ReadCursor,
+        function: FunctionCode,
+        decode: AppDecodeLevel,
+    ) -> Result<(), RequestError> {
         let response = self.parse_all(cursor)?;
-        
+
         if decode.data_headers() {
             tracing::info!("PDU RX - {} {}", function, response);
         } else if decode.header() {
@@ -60,12 +131,12 @@ where
         Ok(())
     }
 
-    fn parse_all(&self, mut cursor: ReadCursor) -> Result<AddressRange, RequestError>{
-        let response = AddressRange::parse(&mut cursor)?;
-        cursor.expect_empty()?;
-        if self.request.read_range != response {
-            return Err(AduParseError::ReplyEchoMismatch.into());
+    fn parse_all(&self, mut cursor: ReadCursor) -> Result<AddressRange, RequestError> {
+        let range = AddressRange::parse(&mut cursor)?;
+        if range != self.request.read_range {
+            return Err(RequestError::BadResponse(AduParseError::ReplyEchoMismatch));
         }
-        Ok(response)
+        cursor.expect_empty()?;
+        Ok(range)
     }
 }
