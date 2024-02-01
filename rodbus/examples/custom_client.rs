076f66cdf3853c997ba9df150e9c7460e9888c33
dkoehler-dev <admin@dominikkoehler.dev>
2024-02-01 20:09:25 +0100

    feat(custom examples): add custom client and server examples for CFC69 (CustomFC) tests

---
+++ rodbus/examples/custom_client.rs
@@ -0,0 +1,287 @@
+use std::error::Error;
+use std::net::{IpAddr, Ipv4Addr};
+use std::process::exit;
+use std::time::Duration;
+
+use tokio_stream::StreamExt;
+use tokio_util::codec::{FramedRead, LinesCodec};
+
+use rodbus::client::*;
+use rodbus::*;
+
+// ANCHOR: runtime_init
+#[tokio::main(flavor = "multi_thread")]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // ANCHOR_END: runtime_init
+
+    // ANCHOR: logging
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_target(false)
+        .init();
+    // ANCHOR_END: logging
+
+    let args: Vec<String> = std::env::args().collect();
+    let transport: &str = match &args[..] {
+        [_, x] => x,
+        _ => {
+            eprintln!("please specify a transport:");
+            eprintln!("usage: outstation <transport> (tcp, rtu, tls-ca, tls-self-signed)");
+            exit(-1);
+        }
+    };
+    match transport {
+        "tcp" => run_tcp().await,
+        #[cfg(feature = "serial")]
+        "rtu" => run_rtu().await,
+        #[cfg(feature = "tls")]
+        "tls-ca" => run_tls(get_ca_chain_config()?).await,
+        #[cfg(feature = "tls")]
+        "tls-self-signed" => run_tls(get_self_signed_config()?).await,
+        _ => {
+            eprintln!(
+                "unknown transport '{transport}', options are (tcp, rtu, tls-ca, tls-self-signed)"
+            );
+            exit(-1);
+        }
+    }
+}
+
+struct LoggingListener;
+impl<T> Listener<T> for LoggingListener
+where
+    T: std::fmt::Debug,
+{
+    fn update(&mut self, value: T) -> MaybeAsync<()> {
+        tracing::info!("Channel Listener: {:?}", value);
+        MaybeAsync::ready(())
+    }
+}
+
+async fn run_tcp() -> Result<(), Box<dyn std::error::Error>> {
+    // ANCHOR: create_tcp_channel
+    let channel = spawn_tcp_client_task(
+        HostAddr::ip(IpAddr::V4(Ipv4Addr::LOCALHOST), 11502),
+        1,
+        default_retry_strategy(),
+        DecodeLevel::default(),
+        Some(Box::new(LoggingListener)),
+    );
+    // ANCHOR_END: create_tcp_channel
+
+    run_channel(channel).await
+}
+
+#[cfg(feature = "serial")]
+async fn run_rtu() -> Result<(), Box<dyn std::error::Error>> {
+    // ANCHOR: create_rtu_channel
+    let channel = spawn_rtu_client_task(
+        "/dev/ttySIM0",                    // path
+        rodbus::SerialSettings::default(), // serial settings
+        1,                                 // max queued requests
+        default_retry_strategy(),          // retry delays
+        DecodeLevel::new(
+            AppDecodeLevel::DataValues,
+            FrameDecodeLevel::Payload,
+            PhysDecodeLevel::Nothing,
+        ),
+        Some(Box::new(LoggingListener)),
+    );
+    // ANCHOR_END: create_rtu_channel
+
+    run_channel(channel).await
+}
+
+#[cfg(feature = "tls")]
+async fn run_tls(tls_config: TlsClientConfig) -> Result<(), Box<dyn std::error::Error>> {
+    // ANCHOR: create_tls_channel
+    let channel = spawn_tls_client_task(
+        HostAddr::ip(IpAddr::V4(Ipv4Addr::LOCALHOST), 11802),
+        1,
+        default_retry_strategy(),
+        tls_config,
+        DecodeLevel::new(
+            AppDecodeLevel::DataValues,
+            FrameDecodeLevel::Nothing,
+            PhysDecodeLevel::Nothing,
+        ),
+        Some(Box::new(LoggingListener)),
+    );
+    // ANCHOR_END: create_tls_channel
+
+    run_channel(channel).await
+}
+
+#[cfg(feature = "tls")]
+fn get_self_signed_config() -> Result<TlsClientConfig, Box<dyn std::error::Error>> {
+    use std::path::Path;
+    // ANCHOR: tls_self_signed_config
+    let tls_config = TlsClientConfig::self_signed(
+        Path::new("./certs/self_signed/entity2_cert.pem"),
+        Path::new("./certs/self_signed/entity1_cert.pem"),
+        Path::new("./certs/self_signed/entity1_key.pem"),
+        None, // no password
+        MinTlsVersion::V1_2,
+    )?;
+    // ANCHOR_END: tls_self_signed_config
+
+    Ok(tls_config)
+}
+
+#[cfg(feature = "tls")]
+fn get_ca_chain_config() -> Result<TlsClientConfig, Box<dyn std::error::Error>> {
+    use std::path::Path;
+    // ANCHOR: tls_ca_chain_config
+    let tls_config = TlsClientConfig::full_pki(
+        Some("test.com".to_string()),
+        Path::new("./certs/ca_chain/ca_cert.pem"),
+        Path::new("./certs/ca_chain/client_cert.pem"),
+        Path::new("./certs/ca_chain/client_key.pem"),
+        None, // no password
+        MinTlsVersion::V1_2,
+    )?;
+    // ANCHOR_END: tls_ca_chain_config
+
+    Ok(tls_config)
+}
+
+fn print_read_result<T>(result: Result<Vec<Indexed<T>>, RequestError>)
+where
+    T: std::fmt::Display,
+{
+    match result {
+        Ok(coils) => {
+            for bit in coils {
+                println!("index: {} value: {}", bit.index, bit.value);
+            }
+        }
+        Err(rodbus::RequestError::Exception(exception)) => {
+            println!("Modbus exception: {exception}");
+        }
+        Err(err) => println!("read error: {err}"),
+    }
+}
+
+fn print_write_result<T>(result: Result<T, RequestError>) {
+    match result {
+        Ok(_) => {
+            println!("write successful");
+        }
+        Err(rodbus::RequestError::Exception(exception)) => {
+            println!("Modbus exception: {exception}");
+        }
+        Err(err) => println!("writer error: {err}"),
+    }
+}
+
+async fn run_channel(mut channel: Channel) -> Result<(), Box<dyn std::error::Error>> {
+    channel.enable().await?;
+
+    // ANCHOR: request_param
+    let params = RequestParam::new(UnitId::new(1), Duration::from_secs(1));
+    // ANCHOR_END: request_param
+
+    let mut reader = FramedRead::new(tokio::io::stdin(), LinesCodec::new());
+    loop {
+        match reader.next().await.unwrap()?.as_str() {
+            "x" => return Ok(()),
+            "ec" => {
+                // enable channel
+                channel.enable().await?;
+            }
+            "dc" => {
+                // disable channel
+                channel.disable().await?;
+            }
+            "ed" => {
+                // enable decoding
+                channel
+                    .set_decode_level(DecodeLevel::new(
+                        AppDecodeLevel::DataValues,
+                        FrameDecodeLevel::Header,
+                        PhysDecodeLevel::Length,
+                    ))
+                    .await?;
+            }
+            "dd" => {
+                // disable decoded
+                channel.set_decode_level(DecodeLevel::nothing()).await?;
+            }
+            "rc" => {
+                // ANCHOR: read_coils
+                let result = channel
+                    .read_coils(params, AddressRange::try_from(0, 5).unwrap())
+                    .await;
+                // ANCHOR_END: read_coils
+                print_read_result(result);
+            }
+            "rdi" => {
+                let result = channel
+                    .read_discrete_inputs(params, AddressRange::try_from(0, 5).unwrap())
+                    .await;
+                print_read_result(result);
+            }
+            "rhr" => {
+                let result = channel
+                    .read_holding_registers(params, AddressRange::try_from(0, 5).unwrap())
+                    .await;
+                print_read_result(result);
+            }
+            "rir" => {
+                let result = channel
+                    .read_input_registers(params, AddressRange::try_from(0, 5).unwrap())
+                    .await;
+                print_read_result(result);
+            }
+            "wsc" => {
+                // ANCHOR: write_single_coil
+                let result = channel
+                    .write_single_coil(params, Indexed::new(0, true))
+                    .await;
+                // ANCHOR_END: write_single_coil
+                print_write_result(result);
+            }
+            "wsr" => {
+                let result = channel
+                    .write_single_register(params, Indexed::new(0, 76))
+                    .await;
+                print_write_result(result);
+            }
+            "wmc" => {
+                let result = channel
+                    .write_multiple_coils(
+                        params,
+                        WriteMultiple::from(0, vec![true, false]).unwrap(),
+                    )
+                    .await;
+                print_write_result(result);
+            }
+            "wmr" => {
+                // ANCHOR: write_multiple_registers
+                let result = channel
+                    .write_multiple_registers(
+                        params,
+                        WriteMultiple::from(0, vec![0xCA, 0xFE]).unwrap(),
+                    )
+                    .await;
+                print_write_result(result);
+                // ANCHOR_END: write_multiple_registers
+            }
+            "scfc" => {
+                // ANCHOR: send_custom_function_code
+                let length = 0x04 as usize;
+                let values = [0xC0, 0xDE, 0xCA, 0xFE]; // i.e.: Voltage Hi = 0xC0 / Voltage Lo = 0xDE / Current Hi = 0xCA / Current Lo = 0xFE
+
+                let result = channel
+                    .send_custom_function_code(
+                        params,
+                        CustomFunctionCode::new(length, values)
+                    )
+                    .await;
+                print_write_result(result);
+                // ANCHOR_END: send_custom_function_code
+            }
+            _ => println!("unknown command"),
+        }
+    }
+}
---
+++ rodbus/examples/custom_server.rs
@@ -0,0 +1,330 @@
+use std::process::exit;
+
+use tokio_stream::StreamExt;
+use tokio_util::codec::{FramedRead, LinesCodec};
+
+use rodbus::server::*;
+use rodbus::*;
+
+struct SimpleHandler {
+    coils: Vec<bool>,
+    discrete_inputs: Vec<bool>,
+    holding_registers: Vec<u16>,
+    input_registers: Vec<u16>,
+}
+
+impl SimpleHandler {
+    fn new(
+        coils: Vec<bool>,
+        discrete_inputs: Vec<bool>,
+        holding_registers: Vec<u16>,
+        input_registers: Vec<u16>,
+    ) -> Self {
+        Self {
+            coils,
+            discrete_inputs,
+            holding_registers,
+            input_registers,
+        }
+    }
+
+    fn coils_as_mut(&mut self) -> &mut [bool] {
+        self.coils.as_mut_slice()
+    }
+
+    fn discrete_inputs_as_mut(&mut self) -> &mut [bool] {
+        self.discrete_inputs.as_mut_slice()
+    }
+
+    fn holding_registers_as_mut(&mut self) -> &mut [u16] {
+        self.holding_registers.as_mut_slice()
+    }
+
+    fn input_registers_as_mut(&mut self) -> &mut [u16] {
+        self.input_registers.as_mut_slice()
+    }
+}
+
+// ANCHOR: request_handler
+impl RequestHandler for SimpleHandler {
+    fn read_coil(&self, address: u16) -> Result<bool, ExceptionCode> {
+        self.coils.get(address as usize).to_result()
+    }
+
+    fn read_discrete_input(&self, address: u16) -> Result<bool, ExceptionCode> {
+        self.discrete_inputs.get(address as usize).to_result()
+    }
+
+    fn read_holding_register(&self, address: u16) -> Result<u16, ExceptionCode> {
+        self.holding_registers.get(address as usize).to_result()
+    }
+
+    fn read_input_register(&self, address: u16) -> Result<u16, ExceptionCode> {
+        self.input_registers.get(address as usize).to_result()
+    }
+
+    fn write_single_coil(&mut self, value: Indexed<bool>) -> Result<(), ExceptionCode> {
+        tracing::info!(
+            "write single coil, index: {} value: {}",
+            value.index,
+            value.value
+        );
+
+        if let Some(coil) = self.coils.get_mut(value.index as usize) {
+            *coil = value.value;
+            Ok(())
+        } else {
+            Err(ExceptionCode::IllegalDataAddress)
+        }
+    }
+
+    fn process_custom_function_code(&mut self, values: CustomFunctionCode) -> Result<(), ExceptionCode> {
+        let mut custom_fc_args = [0_u16; 4]; // i.e.: Voltage Hi = 0x02, Voltage Lo = 0x03, Current Hi = 0x04, Current Lo = 0x05
+        for (i, &value) in values.iter().enumerate() {
+            custom_fc_args[i] = value;
+        }
+        tracing::info!("processing custom function code arguments: {:?}", custom_fc_args);
+
+        Ok(())
+    }
+
+    fn write_single_register(&mut self, value: Indexed<u16>) -> Result<(), ExceptionCode> {
+        tracing::info!(
+            "write single register, index: {} value: {}",
+            value.index,
+            value.value
+        );
+
+        if let Some(reg) = self.holding_registers.get_mut(value.index as usize) {
+            *reg = value.value;
+            Ok(())
+        } else {
+            Err(ExceptionCode::IllegalDataAddress)
+        }
+    }
+
+    fn write_multiple_coils(&mut self, values: WriteCoils) -> Result<(), ExceptionCode> {
+        tracing::info!("write multiple coils {:?}", values.range);
+
+        let mut result = Ok(());
+
+        for value in values.iterator {
+            if let Some(coil) = self.coils.get_mut(value.index as usize) {
+                *coil = value.value;
+            } else {
+                result = Err(ExceptionCode::IllegalDataAddress)
+            }
+        }
+
+        result
+    }
+
+    fn write_multiple_registers(&mut self, values: WriteRegisters) -> Result<(), ExceptionCode> {
+        tracing::info!("write multiple registers {:?}", values.range);
+
+        let mut result = Ok(());
+
+        for value in values.iterator {
+            if let Some(reg) = self.holding_registers.get_mut(value.index as usize) {
+                *reg = value.value;
+            } else {
+                result = Err(ExceptionCode::IllegalDataAddress)
+            }
+        }
+
+        result
+    }
+}
+// ANCHOR_END: request_handler
+
+#[tokio::main(flavor = "multi_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // initialize logging
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_target(false)
+        .init();
+
+    let args: Vec<String> = std::env::args().collect();
+    let transport: &str = match &args[..] {
+        [_, x] => x,
+        _ => {
+            eprintln!("please specify a transport:");
+            eprintln!("usage: outstation <transport> (tcp, rtu, tls-ca, tls-self-signed)");
+            exit(-1);
+        }
+    };
+    match transport {
+        "tcp" => run_tcp().await,
+        #[cfg(feature = "serial")]
+        "rtu" => run_rtu().await,
+        #[cfg(feature = "tls")]
+        "tls-ca" => run_tls(get_ca_chain_config()?).await,
+        #[cfg(feature = "tls")]
+        "tls-self-signed" => run_tls(get_self_signed_config()?).await,
+        _ => {
+            eprintln!(
+                "unknown transport '{transport}', options are (tcp, rtu, tls-ca, tls-self-signed)"
+            );
+            exit(-1);
+        }
+    }
+}
+
+async fn run_tcp() -> Result<(), Box<dyn std::error::Error>> {
+    let (handler, map) = create_handler();
+
+    // ANCHOR: tcp_server_create
+    let server = rodbus::server::spawn_tcp_server_task(
+        1,
+        "127.0.0.1:11502".parse()?,
+        map,
+        AddressFilter::Any,
+        DecodeLevel::default(),
+    )
+    .await?;
+    // ANCHOR_END: tcp_server_create
+
+    run_server(server, handler).await
+}
+
+#[cfg(feature = "serial")]
+async fn run_rtu() -> Result<(), Box<dyn std::error::Error>> {
+    let (handler, map) = create_handler();
+
+    // ANCHOR: rtu_server_create
+    let server = rodbus::server::spawn_rtu_server_task(
+        "/dev/ttySIM1",
+        rodbus::SerialSettings::default(),
+        default_retry_strategy(),
+        map,
+        DecodeLevel::new(
+            AppDecodeLevel::DataValues,
+            FrameDecodeLevel::Payload,
+            PhysDecodeLevel::Data,
+        ),
+    )?;
+    // ANCHOR_END: rtu_server_create
+
+    run_server(server, handler).await
+}
+
+#[cfg(feature = "tls")]
+async fn run_tls(tls_config: TlsServerConfig) -> Result<(), Box<dyn std::error::Error>> {
+    let (handler, map) = create_handler();
+
+    // ANCHOR: tls_server_create
+    let server = rodbus::server::spawn_tls_server_task_with_authz(
+        1,
+        "127.0.0.1:11802".parse()?,
+        map,
+        ReadOnlyAuthorizationHandler::create(),
+        tls_config,
+        AddressFilter::Any,
+        DecodeLevel::default(),
+    )
+    .await?;
+    // ANCHOR_END: tls_server_create
+
+    run_server(server, handler).await
+}
+
+fn create_handler() -> (
+    ServerHandlerType<SimpleHandler>,
+    ServerHandlerMap<SimpleHandler>,
+) {
+    // ANCHOR: handler_map_create
+    let handler =
+        SimpleHandler::new(vec![false; 10], vec![false; 10], vec![0; 10], vec![0; 10]).wrap();
+
+    // map unit ids to a handler for processing requests
+    let map = ServerHandlerMap::single(UnitId::new(1), handler.clone());
+    // ANCHOR_END: handler_map_create
+
+    (handler, map)
+}
+
+#[cfg(feature = "tls")]
+fn get_self_signed_config() -> Result<TlsServerConfig, Box<dyn std::error::Error>> {
+    use std::path::Path;
+    // ANCHOR: tls_self_signed_config
+    let tls_config = TlsServerConfig::new(
+        Path::new("./certs/self_signed/entity1_cert.pem"),
+        Path::new("./certs/self_signed/entity2_cert.pem"),
+        Path::new("./certs/self_signed/entity2_key.pem"),
+        None, // no password
+        MinTlsVersion::V1_2,
+        CertificateMode::SelfSigned,
+    )?;
+    // ANCHOR_END: tls_self_signed_config
+
+    Ok(tls_config)
+}
+
+#[cfg(feature = "tls")]
+fn get_ca_chain_config() -> Result<TlsServerConfig, Box<dyn std::error::Error>> {
+    use std::path::Path;
+    // ANCHOR: tls_ca_chain_config
+    let tls_config = TlsServerConfig::new(
+        Path::new("./certs/ca_chain/ca_cert.pem"),
+        Path::new("./certs/ca_chain/server_cert.pem"),
+        Path::new("./certs/ca_chain/server_key.pem"),
+        None, // no password
+        MinTlsVersion::V1_2,
+        CertificateMode::AuthorityBased,
+    )?;
+    // ANCHOR_END: tls_ca_chain_config
+
+    Ok(tls_config)
+}
+
+async fn run_server(
+    mut server: ServerHandle,
+    handler: ServerHandlerType<SimpleHandler>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut reader = FramedRead::new(tokio::io::stdin(), LinesCodec::new());
+    loop {
+        match reader.next().await.unwrap()?.as_str() {
+            "x" => return Ok(()),
+            "ed" => {
+                // enable decoding
+                server
+                    .set_decode_level(DecodeLevel::new(
+                        AppDecodeLevel::DataValues,
+                        FrameDecodeLevel::Header,
+                        PhysDecodeLevel::Length,
+                    ))
+                    .await?;
+            }
+            "dd" => {
+                // disable decoding
+                server.set_decode_level(DecodeLevel::nothing()).await?;
+            }
+            "uc" => {
+                let mut handler = handler.lock().unwrap();
+                for coil in handler.coils_as_mut() {
+                    *coil = !*coil;
+                }
+            }
+            "udi" => {
+                let mut handler = handler.lock().unwrap();
+                for discrete_input in handler.discrete_inputs_as_mut() {
+                    *discrete_input = !*discrete_input;
+                }
+            }
+            "uhr" => {
+                let mut handler = handler.lock().unwrap();
+                for holding_register in handler.holding_registers_as_mut() {
+                    *holding_register += 1;
+                }
+            }
+            "uir" => {
+                let mut handler = handler.lock().unwrap();
+                for input_register in handler.input_registers_as_mut() {
+                    *input_register += 1;
+                }
+            }
+            _ => println!("unknown command"),
+        }
+    }
+}
