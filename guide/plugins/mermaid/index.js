bf8a186086e5e4564d7446e37c74fcc51ebfb3a1
Nathan Lee <nslee333@gmail.com>
2025-07-10 14:41:59 -0700

    deleted leftover unused code

--- guide/plugins/mermaid/index.js
+++ guide/plugins/mermaid/index.js
@@ -7,13 +7,6 @@ module.exports = function mermaid(options = {}) {
         visit(tree, 'code', (node, index, parent) => {
             if(node.lang === 'mermaid') {
                 node.type = 'jsx';
-                // Properly escape the chart content for JSX
-                const escapedChart = node.value
-                    .replace(/\\/g, '\\\\')
-                    .replace(/"/g, '\\"')
-                    .replace(/\n/g, '\\n')
-                    .replace(/\r/g, '\\r');
-                node.value = `<Mermaid chart={"${escapedChart}"} />`;
 
                 if(!importAdded) {
                     const importPath = path.relative(file.dirname, path.resolve(__dirname, '../../src/theme/Mermaid.js')).replace(/\\/g, '/');
